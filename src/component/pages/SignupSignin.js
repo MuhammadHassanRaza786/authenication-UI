30eee3ba562e57ccb3f4d6c5c3dcec036b759ca2
hunainparekh2211006KHICND <hunain.parekh@xloopdigital.com>
2023-01-24 18:27:18 +0500

    Linkedin Authentication In Process

--- src/component/pages/SignupSignin.js
+++ src/component/pages/SignupSignin.js
@@ -6,6 +6,7 @@ import signin from './img/signin.svg';
 import signup from './img/signup.svg';
 import './css/signupandsignin.css';
 import swal from 'sweetalert';
+import { useLinkedIn } from 'react-linkedin-login-oauth2';
 
 
 
@@ -63,6 +64,16 @@ function SignupSignin() {
     }
     return errors;
   }
+    const { linkedInLogin } = useLinkedIn({
+      clientId: '775lxvvy64z4lo',
+      redirectUri: `https://www.linkedin.com/developers/tools/oauth/redirect`, // for Next.js, you can use `${typeof window === 'object' && window.location.origin}/linkedin`
+      onSuccess: (code) => {
+        console.log(code);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
 
 
   const handleSubmit = (event) => {
@@ -155,7 +166,7 @@ function SignupSignin() {
               <input type="submit" value="Login" className="btn solid" />
               <p className="social-text">Or Sign in with social platforms</p>
               <div className="social_icons">
-                <a href="#"><i className="fa-brands fa-linkedin"></i></a>
+                <a href="#" onClick={linkedInLogin}><i className="fa-brands fa-linkedin"></i></a>
                 <a href="#"><i className="fa-brands fa-microsoft"></i></a>
                 <a href="#"><i className="fa-brands fa-google"></i></a>
               </div>
